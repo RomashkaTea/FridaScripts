e817d71e5a0cf11cc40bce43f4ade10112660bfd
Romashka <80623570+RomashkaTea@users.noreply.github.com>
2022-02-03 17:43:35 +0300

    Create script.js

---
+++ Boom Beach/OfflineMode/19.60/script.js
@@ -0,0 +1,12 @@
+var base = Module.findBaseAddress("libg.so");
+
+base.add(0x3D4BE4).writeU8(1);
+
+Java.perform(function () { 
+    var context = Java.use('android.app.ActivityThread').currentApplication().getApplicationContext();
+
+    Java.scheduleOnMainThread(function() {
+            var toast = Java.use("android.widget.Toast");
+            toast.makeText(Java.use("android.app.ActivityThread").currentApplication().getApplicationContext(), Java.use("java.lang.String").$new("Offline Mode for Boom Beach 19.60 By Romashka"), 1).show();
+    });
+});
