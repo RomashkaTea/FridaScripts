71f3403df61ae062594d8e522025e38236b2c664
Romashka <80623570+RomashkaTea@users.noreply.github.com>
2022-02-03 17:42:35 +0300

    Create script_x86.js

---
+++ Boom Beach/OfflineMode/18.136/script_x86.js
@@ -0,0 +1,12 @@
+var base = Module.findBaseAddress("libg.so");
+
+base.add(0x25C39A).writeU8(1); 
+
+Java.perform(function () { 
+    var context = Java.use('android.app.ActivityThread').currentApplication().getApplicationContext();
+
+    Java.scheduleOnMainThread(function() {
+            var toast = Java.use("android.widget.Toast");
+            toast.makeText(Java.use("android.app.ActivityThread").currentApplication().getApplicationContext(), Java.use("java.lang.String").$new("FRIDA BY ROMASHKA!"), 1).show();
+    });
+});
